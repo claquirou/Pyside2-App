6a4e1585d16a370bd6ba73b9be4a58ba52b5bf23
Franck Herve <Herve1774@gmail.com>
2020-01-12 04:20:42 +0100

    Delete file

--- PyNotes/src/main/freeze.sh
+++
@@ -1,4 +0,0 @@
-source /home/claquirou/Bureau/Project/PySide2-App/env/bin/activate
-cd /home/claquirou/Bureau/Project/PySide2-App/PyNotes/
-fbs clean
-fbs freeze
--- PyTasks/src/main/freeze.sh
+++
@@ -1,4 +0,0 @@
-source /home/claquirou/Bureau/Project/PySide2-App/env/bin/activate
-cd /home/claquirou/Bureau/Project/PySide2-App/PyTasks/
-fbs clean
-fbs freeze
